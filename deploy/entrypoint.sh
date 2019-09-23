dadab9b98f2bfce661cc7150b2e9552fd20073e9
Alex Harden <alex.harden@allscripts.com>
2019-09-23 11:15:19 -0400

    Update path

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 : ${AWS_REGION:=us-east-1}
 : ${DOTNET_LAMBDA_PACKAGE_NAME:=latest.zip}
+export PATH="$PATH:/root/.dotnet/tools"
 cd "${DOTNET_LAMBDA_WORKING_DIR:-.}"
 dotnet lambda package $DOTNET_LAMBDA_PACKAGE_NAME
 aws s3 cp --only-show-errors $DOTNET_LAMBDA_PACKAGE_NAME s3://$DOTNET_LAMBDA_S3_LOCATION/$DOTNET_LAMBDA_PACKAGE_NAME
